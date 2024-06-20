cf9bca57488d4593af9f7ee200b6b8dcd70d6044
Zhaoyang Li <lzy445850071@gmail.com>
2024-06-20 16:40:16 +0800

    Revert "feat: use pdm as package manager"

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
-logger = logging.getLogger("backport")
-logger.setLevel(os.getenv("LOG_LEVEL", "INFO").upper())
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger('backport')
+logger.setLevel(os.getenv("LOG_LEVEL", 'INFO').upper())
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 logger.addHandler(ch)
+
